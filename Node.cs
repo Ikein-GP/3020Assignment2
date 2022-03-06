3163c5712ff7a9efa501f85fe684d664d1b18ef4
Greg Prouty <Gregoryprouty@trentu.ca>
2022-03-06 18:39:26 -0500

    Add project files.

---
+++ Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+class Node
+{
+    public string? Data { get; set; }
+    public Node? Left { get; set; }
+    public Node? Right { get; set; }
+    public int LeftChars { get; set; }
+
+    public Node() 
+    {
+        LeftChars = 0;
+    }
+
+    public Node(string data) 
+    {
+        Data = data;
+        LeftChars = 0;
+    }
+}
+
---
+++ Rope.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Rope 
+{
+    public Node root;
+
+    public Rope() 
+    {
+        root = new Node();
+    }
+
+    void Insert(String S, int i) 
+    {
+        Node current = root;
+        if (S.Length > 10) //if the given string is too long
+        {
+            Console.WriteLine("String must be 10 characters or less");
+        }
+        else if (current.LeftChars == 0) //check to see if the rope is empty
+        {
+            root.Left = new Node(S); //if the rope is empty the specified index is irrelevant and the new node should just go to the left
+            root.LeftChars = S.Length;
+        }
+        while (current.Left != null) 
+        {
+            if (current.LeftChars > i) 
+            {
+                i = i - current.LeftChars;
+                if (current.Right == null)
+                {
+                    current.Right = new Node(S);
+                }
+                else
+                {
+                    current = current.Right;
+                }
+            }
+        }
+    }
+
+}
